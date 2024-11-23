a8ebc8f3e4c4d059ca1a4b4253b8977556b2e088
Emin Taşdelen <emintsdelen4114@gmail.com>
2024-11-23 14:55:48 +0300

    Enable chatbot to read images via URL

--- api.py
+++ api.py
@@ -1,14 +1,10 @@
-import replicate
-import os
-import io
-import sys
+import replicate, os, io, sys
 from dotenv import load_dotenv
 
-def Api(prompt):
+def chatApi(prompt):
     first_item_skipped = False
     api_token = os.getenv("REPLICATE_API_TOKEN")
     replicate.Client(api_token=api_token)
-    model_name = "meta/meta-llama-3-8b-instruct"
 
     with open("conversation.txt", "r", encoding="utf-8") as conversation:
         conversationText = conversation.read()
@@ -16,16 +12,30 @@ def Api(prompt):
     output = io.StringIO()
     sys.stdout = output
     for event in replicate.stream(
-        model_name,
+        "meta/meta-llama-3-8b-instruct",
         input={
             "prompt": prompt,
-            "system_prompt": "You are a helpful assistant, this is the conversation with user: " + conversationText,
+            "system_prompt": "You are a helpful assistant and you can recognize images by their URL (to do that user should type '0' and send it, after that user should provide image URL), this is the conversation with user: " + conversationText,
             "length_penalty": 1
         },
     ):
         if not first_item_skipped:
             first_item_skipped = True
             continue
+        print(event, end="")
+    sys.stdout = sys.__stdout__
+    return output.getvalue().strip()
+
+def imageApi(imageUrl, prompt):
+    output = io.StringIO()
+    sys.stdout = output
+    for event in replicate.stream(
+        "yorickvp/llava-13b:80537f9eead1a5bfa72d5ac6ea6414379be41d4d4f6679fd776e9535d1eb58bb",
+        input={
+            "image": imageUrl,
+            "prompt": prompt
+        }
+    ):
         print(event, end="")
     sys.stdout = sys.__stdout__
     return output.getvalue().strip()
--- main.py
+++ main.py
@@ -1,15 +1,29 @@
-from api import Api
+from api import chatApi, imageApi
 
 with open("conversation.txt", "w", encoding="utf-8") as conversation:
     conversation.write("")
 def ReplicateMetaAiChatbot():
-    prompt = str(input("\n--> "))
-    with open("conversation.txt", "a", encoding="utf-8") as conversation:
-        conversation.write("'User: " + prompt + "', ")
-    output = Api(prompt)
-    print("\n" + output)
-    with open("conversation.txt", "a", encoding="utf-8") as conversation:
-        conversation.write("'Chatbot: " + output + "', ")
+    print("\nTo upload an image type '0'")
+    prompt = str(input("--> "))
+    if prompt == "0":
+        imageUrl = input("\nImage url --> ")
+        prompt = input("Prompt for image (You can leave this blank) --> ")
+        with open("conversation.txt", "a", encoding="utf-8") as conversation:
+            conversation.write("'User: " + imageUrl + " " + prompt + "', ")
+        if prompt == "":
+            prompt = "explain this image."
+        output = imageApi(imageUrl, "Explain this image.")
+        output = chatApi("'" + output + "', " + prompt)
+        print("\n" + output)
+        with open("conversation.txt", "a", encoding="utf-8") as conversation:
+            conversation.write("'Chatbot: " + output + "', ")
+    else:
+        with open("conversation.txt", "a", encoding="utf-8") as conversation:
+            conversation.write("'User: " + prompt + "', ")
+        output = chatApi(prompt)
+        print("\n" + output)
+        with open("conversation.txt", "a", encoding="utf-8") as conversation:
+            conversation.write("'Chatbot: " + output + "', ")
 
 while True:
     if __name__ == "__main__":

d65b9cb44ece859a3ab7f4186229fd980f2679c7
Emin Taşdelen <emintsdelen4114@gmail.com>
2024-11-23 12:32:09 +0300

    Add conversation memory feature and conversation.txt file

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
 import replicate
 import os
+import io
+import sys
 from dotenv import load_dotenv
 
 def Api(prompt):
@@ -8,12 +10,16 @@ def Api(prompt):
     replicate.Client(api_token=api_token)
     model_name = "meta/meta-llama-3-8b-instruct"
 
-    print("")
+    with open("conversation.txt", "r", encoding="utf-8") as conversation:
+        conversationText = conversation.read()
+
+    output = io.StringIO()
+    sys.stdout = output
     for event in replicate.stream(
         model_name,
         input={
             "prompt": prompt,
-            "system_prompt": "You are a helpful assistant",
+            "system_prompt": "You are a helpful assistant, this is the conversation with user: " + conversationText,
             "length_penalty": 1
         },
     ):
@@ -21,4 +27,5 @@ def Api(prompt):
             first_item_skipped = True
             continue
         print(event, end="")
-    print("")
+    sys.stdout = sys.__stdout__
+    return output.getvalue().strip()
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from api import Api
 
+with open("conversation.txt", "w", encoding="utf-8") as conversation:
+    conversation.write("")
 def ReplicateMetaAiChatbot():
     prompt = str(input("\n--> "))
-    Api(prompt)
+    with open("conversation.txt", "a", encoding="utf-8") as conversation:
+        conversation.write("'User: " + prompt + "', ")
+    output = Api(prompt)
+    print("\n" + output)
+    with open("conversation.txt", "a", encoding="utf-8") as conversation:
+        conversation.write("'Chatbot: " + output + "', ")
 
 while True:
     if __name__ == "__main__":
